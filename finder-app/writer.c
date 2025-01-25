534a37d2728282ab2ccd764c15358f158d7117e5
fed67 <you@example.com>
2025-01-25 17:18:58 +0100

    Update writer.c. Update finder-test.sh

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -9,18 +9,16 @@ int main(int argc, char* argv[]) {
     openlog(NULL, 0, LOG_USER);
 
     if (argc != 3) {
-        syslog(LOG_ERR, "Error function must have <file-path> and <text> as given arguments");
+        syslog(LOG_ERR, "Error function must have <writefile> and <writestr> as given arguments");
         closelog();
         exit(1);
     }
 
-    char* path = argv[1];
-    char* data = argv[2];
+    char* writefile = argv[1];
+    char* writestr = argv[2];
     
-    //printf("path %s\n", path);
-    //printf("data %s\n", data);
 
-    FILE *myfile = fopen(path, "w");
+    FILE *myfile = fopen(writefile, "w");
 
 
     if(myfile == NULL) {
@@ -29,9 +27,9 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    fputs(data, myfile);
+    fputs(writestr, myfile);
 
-    syslog(LOG_DEBUG, "Writing %s to %s", data, path);
+    syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
 
 
     fclose(myfile);
